577b26a72d5028f18ff35d6a6fc8e165aa72784a
LeelK <karunarathne1995@gmail.com>
2020-01-26 06:07:23 +0530

    LinkList

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import Array.MyArray;
+import LinkedList.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        MyArray myArray = new MyArray(5);
-
+/*        MyArray myArray = new MyArray(5);
         myArray.insert(10);
         myArray.insert(20);
         myArray.insert(20);
@@ -11,10 +13,19 @@ public class Main {
         myArray.insert(140);
         myArray.insert(140);
         myArray.insert(140);
-
         System.out.println("Size of array is: "+ myArray.size());
-
         System.out.println("Index is: "+myArray.indexOf(140));
-        myArray.print();
+        myArray.print();*/
+
+
+        LinkedList list = new LinkedList();
+        list.insert(18);
+        list.insert(54);
+        list.insert(52);
+        list.insert(15);
+
+        list.show();
+
+
     }
 }
